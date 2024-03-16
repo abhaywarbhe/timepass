c0967229bca3c27aef56e368ed56dbde5e037dde
abhaywarbhe <abhaywarbhe@gmail.com>
2024-03-16 23:39:55 +0530

    just to see

---
+++ src/com/collectiondemo/ArrayListStringDemo.java
@@ -0,0 +1,17 @@
+package com.collectiondemo;
+
+import java.util.ArrayList;
+
+public class ArrayListStringDemo {
+
+	public static void main(String[] args) {
+		ArrayList<Character> al=new ArrayList<Character>();
+		al.add('a');
+		al.add('s');
+		al.add('h');
+		al.add('u');
+		System.out.println(al);
+		
+	}
+
+}
